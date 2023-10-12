ad4b68e6b958225a7b228dd6b14b95bc95b04b48
hangvtaqua <147724458+hangvtaqua@users.noreply.github.com>
2023-10-12 20:59:51 +0700

    Update and rename practice3.sql to buổi_3.sql

---
+++ buổi_3.sql
@@ -0,0 +1,68 @@
+--baitap1
+select name from city
+where countrycode = 'USA'
+and population > 120000 
+  
+--baitap2
+select * from city
+where countrycode='JPN'
+  
+--baitap3
+select city, state from station
+  
+--baitap4
+select city from station
+where city like 'e%'
+or city like 'a%'
+or city like 'i%'
+or city like 'o%'
+or city like 'u%'
+
+--baitap5
+select DISTINCT city from station
+where city like '%a'
+or city like '%e'
+or city like '%i'
+or city like '%o'
+or city like '%u'
+
+--baitap6
+select DISTINCT city from station
+where city not like 'e%'
+and city not like 'a%'
+and city not like 'i%'
+and city not like 'o%'
+and city not like 'u%'
+
+--baitap7
+select name from employee
+order by name ASC
+
+--baitap8
+select name from employee
+where salary > 2000
+and months < 10
+order by employee_id asc
+
+--baitap9
+select product_id from products
+where low_fats = 'Y'
+and  recyclable = 'Y'
+
+--baitap10
+select name from customer
+where referee_id != 2
+or referee_id is NULL
+
+--baitap11
+select name, population, area from world
+where area >= 3000000
+or population >= 25000000
+
+--baitap12
+select DISTINCT author_id as id from views
+where author_id = viewer_id
+order by id;
+
+--baitap13
+
